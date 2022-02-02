28972a2274c4485dec0d869ad65537eb5e5a5252
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2022-02-02 11:33:19 +0900

    Added forced logout when changed username and pw etc

--- server/routes/accountRoutes.ts
+++ server/routes/accountRoutes.ts
@@ -4,6 +4,7 @@ const router = express.Router();
 const AccountController = require("../controllers/accountController");
 const AuthController = require("../controllers/authController");
 
+//
 router.patch("/changeEmail", AuthController.checkLoggedIn, AccountController.changeUserEmail);
 router.patch("/changePassword", AuthController.checkLoggedIn, AccountController.changeUserPassword);
 router.delete("/deleteAccount", AuthController.checkLoggedIn, AccountController.deleteUserAccount);
