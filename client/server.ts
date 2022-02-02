144d0b6265376e5df6c606b0e893a18e0615ac52
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2022-02-02 18:47:59 +0900

    added urls

--- client/server.ts
+++ client/server.ts
@@ -27,6 +27,14 @@ app.get("/auth", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
+app.get("/help", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.get("/payment", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 app.listen(3002, () => {
   console.log("Client hosted on 3002!");
 });
