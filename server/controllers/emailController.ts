69372c6dfcb15171764f4b71533e25977a530631
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2022-02-03 18:24:28 +0900

    added email helper

--- server/controllers/emailController.ts
+++ server/controllers/emailController.ts
@@ -1,15 +1,11 @@
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API);
-
-// ---------
-// Emails are WIP - Need to tigten up the code / process and create better looking, formatted emails.
-//----------
+import * as EmailHelper from "../services/emailServices";
 
 // Using SendGrid and its API..
 const sendMail = async (message: Object) => {
   try {
     const sendMail = await sgMail.send(message);
-    console.log(sendMail);
   } catch (error: any) {
     console.error(error);
     if (error.response) {
@@ -23,16 +19,11 @@ export const sendWelcomeEmail = async (username: string, email: string, activate
     to: `${email}`, // Change to your recipient
     from: process.env.ADMIN_EMAIL, // Change to your verified sender
     subject: `Welcome to Tasky, ${username}!`,
-    text: `Welcome! ${username} to my Tasky task recording app!.. (Email design in progress). Please go to the following link to activate your account: https://tasky.russell-carey.com/auth?=${activateString}.`,
-    html: `<strong>Welcome! ${username} to Tasky.</strong>
-    <br>
-    <p>Email design in progress</p>
-    <br>
-    <p> Please click <a href="https://tasky.russell-carey.com/auth?=${activateString}">here</a> to confirm and activate your account.`,
+    text: EmailHelper.prepareSignupSuccessText(username, email, activateString),
+    html: EmailHelper.prepareSignupSuccessText(username, email, activateString),
   };
 
   await sendMail(msg);
-
   return msg;
 };
 
@@ -41,15 +32,23 @@ export const sendPaymentEmailSuccess = async (username: string, email: string) =
     to: `${email}`, // Change to your recipient
     from: process.env.ADMIN_EMAIL, // Change to your verified sender
     subject: `Upgrade ahoooy, ${username}!`,
-    text: `${username}, This is just to let you know that your upgrade was successful. Your payment has gone through and your account has been upgraded! If you have any issues, please contact us!`,
-    html: `<strong>Upgrade ahoy! ${username}.</strong><br>
-    This is just to let you know that your upgrade was successful. <br>
-    Your payment has gone through and your account has been upgraded!<br>
-    If you have any issues, please contact us!<br><br>
-    Thank you for using tasky!`,
+    text: EmailHelper.preparePaymentSuccessText(username, email),
+    html: EmailHelper.preparePaymentSuccessHTML(username, email),
   };
 
   await sendMail(msg);
+  return msg;
+};
 
+export const sendPaymentEmailFailure = async (username: string, email: string) => {
+  const msg = {
+    to: `${email}`, // Change to your recipient
+    from: process.env.ADMIN_EMAIL, // Change to your verified sender
+    subject: `Upgrade ahoooy, ${username}!`,
+    text: EmailHelper.preparePaymentFailureText(username, email),
+    html: EmailHelper.preparePaymentFailureHTML(username, email),
+  };
+
+  await sendMail(msg);
   return msg;
 };
---
+++ server/services/emailServices.ts
@@ -0,0 +1,35 @@
+export const prepareSignupSuccessText = (username: string, email: string, activateString: string) => {
+  return `Welcome! ${username} to my Tasky task recording app!.. (Email design in progress). Please go to the following link to activate your account: https://tasky.russell-carey.com/auth?=${activateString}.`;
+};
+
+export const prepareSignupSuccessHTML = (username: string, email: string, activateString: string) => {
+  return `<strong>Welcome! ${username} to Tasky.</strong>
+    <br>
+    <p>Email design in progress</p>
+    <br>
+    <p> Please click <a href="https://tasky.russell-carey.com/auth?=${activateString}">here</a> to confirm and activate your account.`;
+};
+
+export const preparePaymentSuccessText = (username: string, email: string) => {
+  return `${username}, This is just to let you know that your upgrade was successful. Your payment has gone through and your account has been upgraded! If you have any issues, please contact us!`;
+};
+
+export const preparePaymentSuccessHTML = (username: string, email: string) => {
+  return `<strong>Upgrade ahoy! ${username}.</strong><br>
+    This is just to let you know that your upgrade was successful. <br>
+    Your payment has gone through and your account has been upgraded!<br>
+    If you have any issues, please contact us!<br><br>
+    Thank you for using tasky!`;
+};
+
+export const preparePaymentFailureText = (username: string, email: string) => {
+  return `${username}, This is just to let you know that your upgrade was not successful. Your payment may have gone through. If you have any issues, please contact us!`;
+};
+
+export const preparePaymentFailureHTML = (username: string, email: string) => {
+  return `<strong>Upgrade ahoy! ${username}.</strong><br>
+    This is just to let you know that your upgrade may not have been successful. <br>
+    Your payment may have gone through.<br>
+    If you have any issues, please contact us!<br><br>
+    Thank you for using tasky!`;
+};
