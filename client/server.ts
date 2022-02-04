d85ed8fb9e46fdce75f9948362a38e8fe2269f34
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2022-02-04 12:48:05 +0900

    Fixing route

--- client/server.ts
+++ client/server.ts
@@ -7,6 +7,10 @@ const app = express();
 // Servied client into pm2 with -spa..
 app.use(express.static(path.join(__dirname, "build")));
 
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 app.get("/console", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
@@ -23,19 +27,11 @@ app.get("/auth", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.get("/docs", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
 app.get("/payment", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.get("/contact", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
