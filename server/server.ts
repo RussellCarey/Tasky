4494857bd7af6344ccbd0b85df58a030c7073154
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2022-06-06 18:36:22 +0900

    Refrshing tokens

--- server/server.ts
+++ server/server.ts
@@ -18,7 +18,7 @@ const app = createServer();
 app.use('/api/tasks', createRouteLimit(60, 60, 'You have made too many requests. Please wait one hour and try again.'));
 app.use('/api/auth/signup', createRouteLimit(60, 2, 'You have made too many signup requests.'));
 
-//Rooooutes.....
+//Rooooutes......
 app.use(!isDev() ? '/taskyapi/auth' : '/api/auth', AuthRoutes);
 app.use(!isDev() ? '/taskyapi/tasks' : '/api/tasks', TasksRoutes);
 app.use(!isDev() ? '/taskyapi/payment' : '/api/payment', PaymentRoutes);
