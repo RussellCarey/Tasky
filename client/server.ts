bad5949bf1a27404405f4ddac0754b190dad25c1
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2022-06-06 17:21:16 +0900

    port change on client

--- client/server.ts
+++ client/server.ts
@@ -1,40 +1,40 @@
 // Host the build!!
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 
 // Prodution Server to run client.
 // Servied client into pm2 with -spa..
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.get("/console", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get('/console', function (req, res) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.get("/account", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get('/account', function (req, res) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.get("/docs", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get('/docs', function (req, res) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.get("/auth", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get('/auth', function (req, res) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.get("/help", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get('/help', function (req, res) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.get("/payment", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get('/payment', function (req, res) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(3002, () => {
-  console.log("Client hosted on 3002!");
+app.listen(3473, () => {
+	console.log('Client hosted on 3002!');
 });
